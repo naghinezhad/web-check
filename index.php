db3d1bf9fdbf7ea844a857ff73323e25f262bf18
MohammadHossein Naghinezhad <114654696+naghinezhad@users.noreply.github.com>
2024-06-29 21:43:23 +0330

    fix directory project

--- index.php
+++ index.php
@@ -5,60 +5,62 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['domain'])) {
 
     $domain = preg_replace('#^https?://#', '', rtrim($domain, '/'));
 
+    $route = './function/';
+
     // Get IP address
-    include 'get_ip.php';
+    include $route . '/get_ip.php';
     $ip = getIpFromDomain($domain);
 
     // Get SSL Chain
-    include 'get_ssl.php';
+    include $route . 'get_ssl.php';
     $sslInfo = getSslChain($domain);
 
     // Get DNS Records
-    include 'get_dns.php';
+    include $route . 'get_dns.php';
     $dnsRecords = getDnsRecords($domain);
 
     // Get Cookies
-    include 'get_cookies.php';
+    include $route . 'get_cookies.php';
     $cookies = getCookies($domain);
 
     // Get Crawl Rules
-    include 'get_crawl_rules.php';
+    include $route . 'get_crawl_rules.php';
     $crawlRules = getCrawlRules($domain);
 
     // Get Headers
-    include 'get_headers.php';
+    include $route . 'get_headers.php';
     $headers = getHeaders($domain);
 
     // Get Quality Metrics
-    include 'get_quality_metrics.php';
+    include $route . 'get_quality_metrics.php';
     $qualityMetrics = getQualityMetrics($domain);
 
     // Get Server Location
-    include 'get_server_location.php';
+    include $route . 'get_server_location.php';
     $serverLocation = getServerLocation($ip);
 
     // Get Associated Hosts
-    include 'get_associated_hosts.php';
+    include $route . 'get_associated_hosts.php';
     $associatedHosts = getAssociatedHosts($domain);
 
     // Get Redirect Chain
-    include 'get_redirect_chain.php';
+    include $route . 'get_redirect_chain.php';
     $redirectChain = getRedirectChain($domain);
 
     // Get TXT Records
-    include 'get_txt_records.php';
+    include $route . 'get_txt_records.php';
     $txtRecords = getTxtRecords($domain);
 
     // Check Server Status
-    include 'check_server_status.php';
+    include $route . 'check_server_status.php';
     $serverStatus = checkServerStatus($domain);
 
     // Check Open Ports
-    include 'check_open_ports.php';
+    include $route . 'check_open_ports.php';
     $openPorts = checkOpenPorts($ip);
 
     // Perform Traceroute
-    include 'traceroute.php';
+    include $route . 'traceroute.php';
     $traceroute = traceroute($domain);
 
     // Response
